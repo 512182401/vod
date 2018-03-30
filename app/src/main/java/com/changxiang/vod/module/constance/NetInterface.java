634b456530ca57b79ac44a6d6847ec6b5af9b133
512182401 <512182401@qq.com>
2018-03-30 16:36:01 +0800

    项目新建

--- app/src/main/java/com/changxiang/vod/module/constance/NetInterface.java
+++ app/src/main/java/com/changxiang/vod/module/constance/NetInterface.java
@@ -43,53 +43,6 @@ public class NetInterface {
     /****** 可添加，上传图片个数 ******/
 
 
-    /******
-     * 微信支付
-     ******/
-    //appid 微信分配的appID
-    public static final String APP_ID = "wxecb544ddccdd0df5";//wx11f4ad30030a2ea6
-    //商户号 微信分配的公众账号ID
-    public static final String MCH_ID = "1493773122";//
-    //  API密钥，在商户平台设置
-    public static final String API_KEY = "8797E4BA5BEE5A425B6A6646B235B76C";//
-
-
-    /**
-     * 支付宝支付业务：入参app_id
-     */
-    public static final String APPID = "2017010904949957";
-    /**
-     * 支付宝账户登录授权业务：入参pid值
-     */
-    public static final String PID = "2088521556267001";
-    /**
-     * 支付宝账户登录授权业务：入参target_id值
-     */
-    public static final String TARGET_ID = "";
-
-    /** 商户私钥，pkcs8格式 */
-    /** 如下私钥，RSA2_PRIVATE 或者 RSA_PRIVATE 只需要填入一个 */
-    /** 如果商户两个都设置了，优先使用 RSA2_PRIVATE */
-    /** RSA2_PRIVATE 可以保证商户交易在更加安全的环境下进行，建议使用 RSA2_PRIVATE */
-    /** 获取 RSA2_PRIVATE，建议使用支付宝提供的公私钥生成工具生成， */
-    /**
-     * 工具地址：https://doc.open.alipay.com/docs/doc.htm?treeId=291&articleId=106097&docType=1
-     */
-    public static final String RSA2_PRIVATE = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCC/Y2T1J3G6RpZfiPiQ3atck4RslDGczhhYVG0a1Q1l9Tn+EO3QJnA0OT+vungmAL1lmat9iPQf2cXYaYgG6M4//9zRQ6y9m5g/PeuZnOTOAzlARR+0T7f9CkZf+VcxE39gBzrcts5b/h4Uxy9KBTIs0MtBeiSsbRNkEVZr6KoyZnZejeU/tKp8kMJKDwHo9M2T3mSPjYZwhsjtxaglF65JDHwV3m6rYUdYd8rmoViI+gBE2ej1s5ly++yWwlkcPjdeh0Czxal2SnHY133Or61aXWTHWktLGQT5+yQ1HB73lO5A9LLLLmEexW2d/bZz9x6ASvG4pkAbbvVb3m7U1x9AgMBAAECggEAMxt+e6k/DfaxAy3GDmfIDBBHCXyLK4KVAtmXqU8fAfIFbaRdt0gLNUHLtuECG1D0BqHx2fnpK1YqAONmj6x42Kp5Hp5ZB9ZCZrEMag0rFsuNrGPpfTli0LpQttRBxxoIW9n6bprXkedwCHJVCjBzO4NFuHFPtryC3qhlhoKSckv3Ba0Y7t8pj5tY0aYhAzZaYFjEVQD9cxA8w8ufEkUfagx46xZlB+OZ5sV3tWE27AwAaXY8E533iFeu7nHVNN1zUjwVLCVOr1DuOVV8jJd5UwoxbMRcLhDxJw7T0uj7xjWzl4lzRtW/neEieP2Xjn9C0qO6EaHGgLLFOY/gqndYwQKBgQDCceyq2AuQ5NB8Uxn7O/z+aEgMCnPgD72OYpqma2YIIh/2RXnY0DaceALwoISoXn1SzP9vx0ANAqqWdW4HftgmLKoeqKW1EDZJioz35tae4/O9hN1JWkpEFM3r5+7/6i0eV1VqLubYJMkQjcxHVYVqTrWOwwpouHfuxW7pOV9BmQKBgQCsdTAN2YXTCFd0cd3FCX2RKJVtbr9wVFP28D4RrZSFab6fMD+469HbgcFh49cWNWt4DfylPxBYrnJFSxhC91TICpVpLNLfGvnxS4SA/quMgBv9OiTiBu7eI6okhj9g1rfl+eOkk+7VdqzAHeCaJpzjruJSp39xeQZhl97SmgWIhQKBgGTnK7KIhI//PXWWxj/KpIH86Y5eCj22zV5LKwe7aepEkyOAcDej7QMLgDMQiWa6Cfzi5pdB5aoavR1w+NUo7M/6k5lvWPZ54EIPhRHMF4nCrySlAxRip3Fm/mkyJaNmCV3aYjfE+QQnKrPNcbl+1soQ6ESaUBeByrL8f47Ft/nZAoGAYWaWS1wtXvD5I8UjsaMYjiFTs8i2yqR1/6PUCnv0xBzjEllXxAM8NTNlmvnpgpuNJPMRW+rwOoXb+BLfkwLJmWmQ1SQBvQwpWgSCw7ASB3pbVHvAVQABbOjLsXE43mKm3nZyAYOjgmBSVPcK4UUpmtYs8O205suptPjnWh6vPVkCgYEAn5yVTMzLi45PB7kpnks6nTw8vXH4saF+6ghiyg/cz4jdRh+NnM0LOYCn8sEs5J1kadPYiECqydtDF458sa9U9h40FE9t8IZ0ZERxgir526+BJlkL4kQMGy3JMT0JaM9IYqdH3GpbVb0R0y7UGJ3NHart3pLkrMVk28f7QSmt5AA=";
-    public static final String RSA_PRIVATE = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAIOdoCVrr/bWnmPE\n" +
-            "aDYoPJSH7cfrtGBTMFQjeE2v73GlTEWPjJNs7T6aTPcmXNeIiyLmLzOL8AgArrn2\n" +
-            "pjgpw04nIetW3ZaiczrRzK3E+zadfEDf4KighWgGeYVgLRfNYirDBnaqQMk3XcvD\n" +
-            "hYoF6hTZWg4q8ABNcvXTI4FLGWtVAgMBAAECgYBdbCp7xWee55KAMK7kGkV+DMo8\n" +
-            "iVN8uC/q2U6QnlxxJ6rvCUj4cG4qbK5LFID8QKC6gfdpOGCF3a4otCoiXYqSTxEU\n" +
-            "untEqS5rtovxvW/fP3TPLZ3DFFIwt2F7np6dvRQ5sLqRvHWNS4r716XDXfxV/Bsf\n" +
-            "lEwaaxAOZ4/6uG1nVQJBAMC28NxyRb/9MdrEufKDjMcuZEnw1TjSJVyuKlS8eSdT\n" +
-            "vq12viH+ZWeFHYcq9U8ZVN0vKEWUBmCHV8qZlyKS2RMCQQCu1j8cMG0bkQuB8WHt\n" +
-            "Nvs80lRXqIMvfsyDco06JjAdUJfULs187qwSrMN2nhwWQf+8gFSPv7YFfqcfAYHR\n" +
-            "F173AkEAqVxcwq9eYwJl3OfErr8zahx4II8ZI61zDkc1hnB4XLp5OULAh2llvps6\n" +
-            "vv5exVvyu8tkrfkPvadT3QYrz0OUpwJBAK2kSV+01Ng5EQXId6rCHXoFpxC8YzYL\n" +
-            "qBCw94SWItkqjvCEXz/CR5Hwldy8IUcV22kax2FRVPVWGaMYuxawMHcCQGir3STI\n" +
-            "2uP/RIcShyA12ZfwzYRbJqtAVt3C991l8piDrf5n0lxtSP1TeFZ/L5zjDjCGEveN\n" +
-            "a5HJuj4re8xhyps=";
 
 
     /**
